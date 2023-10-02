d607e81b52ecfb671bb0ccfac8b101c903150d92
SafaAnwarAlyaaqbeh <134327083+SafaAnwarAlyaaqbeh@users.noreply.github.com>
2023-10-02 20:24:56 +0300

    Update ecom_store.sql

--- DB/ecom_store.sql
+++ DB/ecom_store.sql
@@ -44,7 +44,7 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`admin_id`, `admin_name`, `admin_email`, `admin_pass`, `admin_image`, `admin_contact`, `admin_country`, `admin_job`, `admin_about`) VALUES
-(1, 'aya', 'aa116@gmail.com', '123', 'admin.jpg', '07999999', 'jordan', 'Front-End Developer', '  Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical  ');
+(1, 'safa', 'sa116@gmail.com', '123', 'admin.jpg', '07999999', 'jordan', 'Front-End Developer', '  Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical  ');
 
 -- --------------------------------------------------------
 
